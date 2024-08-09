3baf9679bbae5458621ab28ea0cc4e124b4574d3
Ibrahim Shady <ibrahims.suroor@gmail.com>
2024-08-09 20:10:02 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -45,12 +45,12 @@ function App() {
       name: "Lost and found cargo",
       description: "for those who like to stand out",
       imgLink:
-        "https://prosafety.com/9003-thickbox_default/work-trousers-denim-flexiwork--6956--short-legs.jpg",
+        "https://s3-us-west-2.amazonaws.com/stylyze-neimanmarcus/2022/12/20/63a1c9906d1ead565cd81311",
     },
   ];
 
   return (
-    <div className="App">
+    <div className="App select-none h-full overflow-none">
       <LocomotiveScrollContainer>
         <div data-scroll-sticky className="h-screen w-screen ">
           <div className="h-screen w-screen absolute top-0 z-0 ">
@@ -97,11 +97,13 @@ function App() {
                 className="flex flex-col h-full w-full gap-4 p-2"
                 id={item.id}
               >
+                <div className="h-80 w-full overflow-hidden">
                 <img
                   src={item.imgLink}
                   alt={item.description}
-                  className="aspect-square w-full h-80 object-cover hover:scale-[105%] ease-in-out duration-500"
+                  className="aspect-square w-full h-full object-cover hover:scale-[105%] ease-in-out duration-500"
                 />
+                </div>
                 <div className="flex flex-col gap-0">
                   <h1>{item.name}</h1>
                   <span className="text-xs">{item.description}</span>
@@ -118,7 +120,7 @@ function App() {
             ))}
           </div>
         </div>
-        <div className="text-6xl h-screen border-b-[1px] border-black bg-white">
+        <div className="text-6xl h-screen border-b-[1px] border-black bg-white border-t-[1px] select-none">
           <div className="w-full h-full flex flex-col">
             <div className="h-full w-full border-black border-b-[1px] hover:bg-[#C8C8C8] flex justify-center items-center">
               <h1>MEN</h1>
@@ -131,6 +133,127 @@ function App() {
             </div>
           </div>
         </div>
+ 
+      <div id="infinite" className="font-bold z-10">
+        <div id="infinite-fx" className="ticker-wrap">
+          <span>ESSENSE APPAREL®</span>
+          <span>ESSENSE APPAREL®</span>
+        </div>
+      </div>
+      <div className="h-[120vh] w-screen overflow-hidden">
+        <img src="https://images.pexels.com/photos/7645362/pexels-photo-7645362.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="skating women" className="h-[full] w-screen object-cover"/>
+      </div>
+      <div
+      className=" text-black text-left flex flex-col uppercase pt-16"
+      id="footer"
+    >
+      <div className="contact z-10 font-medium">
+        <ul>
+          <li>
+            <a
+              href="/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Tiktok
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              target="_blank"
+              aria-label="link to instagram"
+              rel="noreferrer"
+            >
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              target="_blank"
+              aria-label="link to twitter"
+              rel="noreferrer"
+            >
+              Twitter
+            </a>
+          </li>
+          <li className="invisible">
+            <a
+              href="/"
+              target="_blank"
+              aria-label="link to twitter"
+              rel="noreferrer"
+            >
+              Twitter
+            </a>
+          </li>
+          <li className="invisible">
+            <a
+              href="/"
+              target="_blank"
+              aria-label="link to twitter"
+              rel="noreferrer"
+            >
+              Twitter
+            </a>
+          </li>
+          <li className=" copyright">
+            <span className="text-xs">@2023 essense APPAREL®</span>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a
+              aria-label="link to work page where all work is displayed"
+              >
+              Sign in
+            </a>
+          </li>
+          <li>
+            <a
+              aria-label="link to cabinet page where all concept projects are displayed"
+            >
+              Offers
+            </a>
+          </li>
+          <li>
+            <a
+              aria-label="link to studio page"
+            >
+              WishList
+            </a>
+          </li>
+          <li>
+            <a
+              aria-label="contact us, let's get in touch"
+            >
+              Cart
+            </a>
+          </li>
+          <li>
+            <a
+              aria-label="contact us, let's get in touch"
+            >
+              Contact Us
+            </a>
+          </li>
+          <li className="copyright">
+              <span className="text-xs">Cookies Policy</span>
+          </li>
+        </ul>
+        <ul>
+          <li className="text-xs underline font-bold">Reach out</li>
+          <li className="text-xs hover:opacity-50 ">
+            <a
+              href="/"
+            >
+              info@essenseapparel.com
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
       </LocomotiveScrollContainer>
     </div>
   );
